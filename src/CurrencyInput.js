d23b796bcf0fe8de39da64e0c9d59e587cf536fe
Aleck-Kit <aleck.kit@protonmail.com>
2021-11-10 09:01:46 +0200

    calculator done

---
+++ src/CurrencyInput.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import './currencyInput.css';
+import PropTypes from 'prop-types';
+
+const CurrencyInput = (props) => {
+    return (
+        <div className="group" >
+            <input type="text" value={props.amount} onChange={event => 
+                props.onAmountChange(event.target.value)}/>
+            <select value={props.currency} onChange={event =>     
+                props.onCurrencyChange(event.target.value)}>
+                {props.currencies.map((currency => (
+                    <option value={currency}>{currency}</option>
+                )))}
+            </select>
+            
+        </div>
+    )
+}
+
+CurrencyInput.propTypes = {
+    amount: PropTypes.number.isRequired,
+    currency:PropTypes.string.isRequired,
+    currencies: PropTypes.array,
+    onAmountChange: PropTypes.func,
+    onCurrencyChange: PropTypes.func
+
+}
+
+export default CurrencyInput
